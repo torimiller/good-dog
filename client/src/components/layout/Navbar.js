94d9da0eba29bacf0113da0e851b43e1e24efd91
Tori Miller <millertori04@gmail.com>
2020-03-24 21:25:46 -0700

    dashboard and profile management

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to='/'>
-            <i className="fas fa-code"></i> DevConnector
+            <i className="fas fa-code"></i> PuppyPal
         </Link>
       </h1>
       { !loading && (<Fragment>{ isAuthenticated ? authLinks: guestLinks }</Fragment>) }
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react'
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -37,6 +39,11 @@ const CreateProfile = props => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history);
+    }
+
     return (
         <Fragment>
         <section className="container">
@@ -48,7 +55,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -137,7 +144,7 @@ const CreateProfile = props => {
 
         
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+        <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
       </form>
       </section>
     </Fragment>
@@ -145,7 +152,8 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+    createProfile: PropTypes.func.isRequired
+};
 
-}
 
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile));
