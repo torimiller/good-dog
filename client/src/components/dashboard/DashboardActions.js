c3eacb316d5ec3886ba5209567d1b1071137329a
Tori Miller <millertori04@gmail.com>
2020-09-22 21:40:52 -0700

    changed primary colors

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const DashboardActions = () => {
     return (
         <div class="dash-buttons">
-        <Link to="/add-goal-in-progress" class="btn btn-light goal-btn"
+        <Link to="/add-goal-in-progress" className="btn btn-light goal-btn"
           ><i class="fas fa-dog text-primary" alt=""></i> Add Goal</Link>
-          <Link to='/edit-profile' class="btn btn-light goal-btn">
-              <i className="fas fa-user-edit" alt=""></i> Edit Profile
+          <Link to='/edit-profile' className="btn btn-light goal-btn">
+              <i className="fas fa-user-edit text-primary" alt=""></i> Edit Profile
           </Link>
       </div>
     )
